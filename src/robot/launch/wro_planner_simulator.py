534d4005e154e5f2f3bcecfc8a5183feb48e8361
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2026-07-05 15:38:33 -0400

    fix x3

--- src/robot/launch/wro_planner_simulator.py
+++ src/robot/launch/wro_planner_simulator.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+import os
+import sys
+current_file_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(current_file_dir)
 from planner.simulator import main
 
 
